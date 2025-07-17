c42421e3a2c7226d62b817742814266c9676dba2
RHalder2001 <rupakhalder999@gmail.com>
2025-07-17 17:01:38 +0000

    all change made

--- WEATHER-APP/script.js
+++ WEATHER-APP/script.js
@@ -7,14 +7,33 @@ const options = {
 	}
 };
 
-const api= async ()=>{
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-} 
+const api = async () => {
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
 
+		
+		
+		// const sky=result.sys
+		// const wind=result.wind
+		const main = result.main;
+		
+		// document.getElementById("feels_like").innerHTML = (main.feels_like - 273.15).toFixed(2) + " °C";
+		// document.getElementById("humidity").innerHTML = main.humidity + " %";
+		// document.getElementById("pressure").innerHTML = main.pressure + " hPa";
+		// document.getElementById("sea_level").innerHTML = main.sea_level + " hPa";
+		document.getElementById("temp").innerHTML = (main.temp - 273.15).toFixed(2) + " °C";
+		document.getElementById("tem").innerHTML = (main.temp - 273.15).toFixed(2) + " °C";
+		 document.getElementById("temp_max").innerHTML = (main.temp_max - 273.15).toFixed(2) + " °C";
+		 document.getElementById("temp_min").innerHTML = (main.temp_min - 273.15).toFixed(2) + " °C";
+		// document.getElementById("deg").innerHTML = wind.deg;
+		// document.getElementById("speed").innerHTML = wind.speed;
+		// document.getElementById("sunrise").innerHTML = sky.sunrise;
+		// document.getElementById("sunset").innerHTML = sky.sunset;
+		
+	} catch (error) {
+		console.error(error);
+	}
+}
 api()
